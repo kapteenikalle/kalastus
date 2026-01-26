504b79055f9a3464a6cce35c565d712bcb539efb
kapteenikalle <makelaeerik2000@gmail.com>
2026-01-26 18:34:49 +0200

    koko homman muokkaus

--- app.py
+++ app.py
@@ -2,16 +2,17 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import requests
 import pandas as pd
-from datetime import datetime, timedelta, time
+from datetime import datetime, combine, time
 
 # --- ASETUKSET ---
 API_KEY = "78c13424469d15c398ab8fa8c832df15"
-# GOOGLE SHEETS -LINKKI:
-SHEET_URL = "https://docs.google.com/spreadsheets/d/14Ri5Ox9gHumxU21yIGyKWXPoc5srXaK7joDcwnUuQ6k/edit?usp=sharing"
+
+try:
+    SHEET_URL = st.secrets["connections"]["gsheets"]["spreadsheet"]
+except:
+    SHEET_URL = "https://docs.google.com/spreadsheets/d/14Ri5Ox9gHumxU21yIGyKWXPoc5srXaK7joDcwnUuQ6k/edit?usp=sharing"
 
 st.set_page_config(page_title="Kalapäiväkirja", page_icon="🎣")
-
-# Yhdistetään Google Sheetsiin
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 def muunna_suunta(asteet):
@@ -31,46 +32,43 @@ def hae_saa(kaupunki):
         }
     except: return None
 
-st.title("Kalapäiväkirja")
+st.title("🎣 Kalapäiväkirja Pro")
 
 with st.form("kalalomake", clear_on_submit=True):
     col1, col2 = st.columns(2)
     with col1:
         paikka = st.text_input("Paikkakunta")
-        lajivaihtoehdot = ["Ahven", "Hauki", "Kuha", "Siika", "Ei saalista", "Muu"]
+        lajivaihtoehdot = ["Ahven", "Hauki", "Kuha", "Siika", "Ei saalista (MP)", "Muu"]
         laji = st.selectbox("Pääasiallinen laji", lajivaihtoehdot)
         pvm = st.date_input("Päivämäärä", datetime.now())
     
     with col2:
-        # Aloitus- ja lopetusajat
         alku = st.time_input("Reissu alkoi", datetime.now().replace(hour=8, minute=0).time())
         loppu = st.time_input("Reissu loppui", datetime.now().time())
         syonti = st.time_input("Paras syöntiaika", datetime.now().time())
-
+        
         st.divider()
         kpl = st.number_input("Kalojen lukumäärä (kpl)", min_value=0, step=1, value=1)
-        paino = st.number_input("Suurimman kalan paino (g)", min_value=0, step=50)
+        paino = st.number_input("Suurimman paino (g)", min_value=0, step=10)
     
-    huomio = st.text_area("Lisatiedot (Viehe, syvyys)")
-    
-    nappi = st.form_submit_button("Tallenna reissu")
+    huomio = st.text_area("Lisatiedot (vieheet, keli, havainnot)")
+    nappi = st.form_submit_button("Tallenna reissu pilveen")
 
 if nappi:
-    if laji and paikka:
-        # Lasketaan kesto tunteina
-        t1 = datetime.combine(datetime.now(), alku)
-        t2 = datetime.combine(datetime.now(), loppu)
+    if paikka:
+        t1 = datetime.combine(pvm, alku)
+        t2 = datetime.combine(pvm, loppu)
         kesto = (t2 - t1).total_seconds() / 3600
         
         if kesto < 0:
-            st.error("Lopetusaika ei voi olla ennen aloitusaikaa!")
+            st.error("Lopetusaika on ennen aloitusaikaa!")
         else:
-            with st.spinner('Haetaan säätietoja ja tallennetaan...'):
+            with st.spinner('Tallennetaan...'):
                 s = hae_saa(paikka)
                 if s:
-                    # Luodaan uusi rivi
+                    # TÄRKEÄÄ: Sanakirjan avaimet on oltava TÄSMÄLLEEN samat kuin Sheetsissä
                     uusi_rivi = pd.DataFrame([{
-                        "Pvm": datetime.now().strftime("%d.%m.%Y"),
+                        "Pvm": pvm.strftime("%d.%m.%Y"),
                         "Aloitusaika": alku.strftime("%H:%M"),
                         "Lopetusaika": loppu.strftime("%H:%M"),
                         "Kesto_h": round(kesto, 1),
@@ -84,28 +82,27 @@ if nappi:
                         "Saa": s["desc"],
                         "Tuuli_ms": s["w_spd"],
                         "Tuulisuunta": muunna_suunta(s["w_deg"]),
-                        "Lisätiedot": huomio
+                        "Lisatiedot": huomio
                     }])
-        
-            
-                    # Luetaan vanhat ja lisätään uusi
+                    
+                    # Luetaan ja päivitetään
                     vanha_data = conn.read(spreadsheet=SHEET_URL)
                     paivitetty_data = pd.concat([vanha_data, uusi_rivi], ignore_index=True)
-            
-                    # Päivitetään Sheets
+                    
+                    # Pidetään vain halutut sarakkeet oikeassa järjestyksessä
+                    sarake_jarjestys = ["Pvm", "Aloitusaika", "Lopetusaika", "Kesto_h", "Syontiaika", "Laji", "Paikka", "Lukumäärä", "Suurin_kala_g", "Lampotila", "Ilmanpaine", "Saa", "Tuuli_ms", "Tuulisuunta", "Lisatiedot"]
+                    paivitetty_data = paivitetty_data[sarake_jarjestys]
+                    
                     conn.update(spreadsheet=SHEET_URL, data=paivitetty_data)
-                    st.success(f"Reissu tallennettu. Sää: {s['temp']}°C")
-                    st.cache_data.clear() # Tyhjennetään välimuisti, jotta uusi rivi näkyy heti
-                else:
-                    st.error("Säätietojen haku epäonnistui.")
+                    st.success(f"Tallennettu onnistuneesti!")
+                    st.cache_data.clear()
     else:
-        st.warning("Syötä paikkakunta ennen tallennusta!")
+        st.warning("Lisää paikkakunta!")
 
-# Näytetään data Sheetsistä
 st.divider()
 try:
     data = conn.read(spreadsheet=SHEET_URL)
-    st.dataframe(data, use_container_width=True)
+    st.subheader("Viimeisimmät merkinnät")
+    st.dataframe(data.tail(10), use_container_width=True)
 except:
-
-    st.warning("Taulukko on vielä tyhjä tai linkki on väärä.")
+    st.info("Taulukko on vielä tyhjä.")
